cbaef2971af6c4d7061f653f54b77dad351d5b09
iSeeGor <iseegor87@gmail.com>
2019-09-17 18:53:26 +0300

    block 1 + Block 2

--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,91 @@
-<img src="../assets/images/fon_glavnaya.png" alt="">
+<main class="main">
+
+    <!--== Hero  -->
+    <section class="section-hero hero">
+        <div class="container hero__container">
+
+            <div class="hero__inner">
+                <h1 class="hero__title">Квартиры от застройщика <br> в солнечной Аланье</h1>
+                <h2 class="hero__subtitle">Мечтаете жить на побережье Средиземного моря?<br> С нашей компанией Ваши мечты становятся реальностью!</h2>
+
+                <button class="button hero__button">Заказать бесплатную консультацию</button>
+            </div>
+
+        </div>
+    </section>
+
+    <!--== Search  -->
+    <section class="section-search search">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <form action="" class="search-form search__area">
+
+                        <div class="search-form__row-top">
+                            <span class="search-form__item">
+                                <div class="search-form__item-title">Статус</div>
+                                <select class="select search-form__select_status">
+                                    <option value="Продажа">Продажа</option>
+                                    <option value="Аренда ">Аренда </option>
+                                    <option selected="" value="Все">Все</option>
+                                </select>
+                            </span>
+
+                            <span class="search-form__item">
+                                <div class="search-form__item-title">Тип</div>
+                                <select class="select search-form__select_type">
+                                    <option value="Квартира">Квартира</option>
+                                    <option value="Пентхаус">Пентхаус</option>
+                                    <option value="Виллы">Виллы</option>
+                                    <option value="Коммерческая">Коммерческая</option>
+                                    <option selected="" value="Все">Все</option>
+                                </select>
+                            </span>
+
+                            <span class="search-form__item">
+                                <div class="search-form__item-title">Комнат</div>
+                                <select class="select search-form__select_rooms">
+                                    <option value="1+1">1+1</option>
+                                    <option value="2+1">2+1</option>
+                                    <option value="3+1">3+1</option>
+                                    <option value="4+1">4+1</option>
+                                    <option value="5+1">5+1</option>
+                                    <option selected="" value="Все">Все</option>
+                                </select>
+                            </span>
+
+                            <span class="search-form__item">
+                                <div class="search-form__item-title">Район</div>
+                                <select class="select search-form__select_area">
+                                    <option value="Аланья">Аланья</option>
+                                    <option value="Авсалар">Авсалар</option>
+                                    <option value="Кыргыджак">Кыргыджак</option>
+                                    <option value="Махмутлар">Махмутлар</option>
+                                    <option value="Оба">Оба</option>
+                                    <option value="Кестель">Кестель</option>
+                                    <option selected="" value="Все">Все</option>
+                                </select>
+                            </span>
+                        </div>    
+
+                        <span class="search-form__item">
+                            <div class="search-form__item-title">Стоимость (€)</div>
+
+                            <input class="input search-form__input" type="number" name="coast-from" placeholder="От" >
+                            <input class="input search-form__input" type="number" name="coast-to" placeholder="До">
+                        </span>
+
+                        <button class="button search-form__button" type="submit">Искать</button>    
+                    </form>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="search-filters search__area">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</main>
