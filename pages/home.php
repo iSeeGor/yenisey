e55ebbf8386b0051f8178818e0658333ef9d2f3a
iSeeGor <iseegor87@gmail.com>
2019-09-18 18:40:02 +0300

    3 Блока на главной

--- pages/home.php
+++ pages/home.php
@@ -16,7 +16,7 @@
 
     <!--== Search  -->
     <section class="section-search search">
-        <div class="container">
+        <div class="container search__container">
             <div class="row">
                 <div class="col-md-8">
                     <form action="" class="search-form search__area">
@@ -81,10 +81,164 @@
 
                 <div class="col-md-4">
                     <div class="search-filters search__area">
-
+                        <ul class="search-filters__list">
+                            <li class="search-filters__item">
+                                <a href="#" class="search-filters__link link" title="Первичная недвижимость">Первичная недвижимость</a>
+                            </li>
+                            <li class="search-filters__item">
+                                <a href="#" class="search-filters__link search-filters__link_active link" title="Недвижимость от застройщика">Недвижимость от застройщика</a>
+                            </li>
+                            <li class="search-filters__item">
+                                <a href="#" class="search-filters__link link" title="Без посредников">Без посредников</a>
+                            </li>
+                            <li class="search-filters__item">
+                                <a href="#" class="search-filters__link link" title="Без посредников">Без посредников</a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
+        </div>
+    </section>
+
+    <!--== About  -->
+    <section class="section-about about">
+        <div class="container">
+            <div class="about__content">
+                <div class="about__text">
+                    <h4 class="about__text-heading">Компания Yenisey</h4> – это не только строительство современных жилых комплексов бизнес-класса, но и бессрочный постпродажный сервис для наших клиентов, которые по праву могут считать себя частью семьи Yenisey.
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--== Video  -->
+    <section class="section-video video">
+        <div class="container">
+            
+            <div class="video__content">
+
+                <div class="video__button-box">
+                    <a href="#" class="video__button" title="play button">
+                        <svg class="video__icon" width="27" height="30" viewBox="0 0 27 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M25.0007 11.4636C27.6674 13.0032 27.6674 16.8522 25.0007 18.3918L6.25073 29.2172C3.58406 30.7568 0.250731 28.8323 0.250731 25.7531L0.250732 4.10242C0.250732 1.02321 3.58407 -0.901287 6.25073 0.638314L25.0007 11.4636Z" fill="white"/>
+                        </svg>
+                    </a>
+                    <span class="video__button-overlay"></span>
+                </div>
+
+                <h2 class="section-title section-video__title">Видео о компании Yenisey</h2>
+            </div>
+        
+        </div>
+    </section>
+
+    <!--== Privilages  -->
+    <section class="section-privilages privilages">
+        <div class="container privilages__container">
+
+            <h2 class="section-title privilages__title">Достоинства жизни в Турции</h2>
+
+            <div class="privilages__content">
+
+                <div class="privilages__item">
+                    <div class="privilages__icon"><img src="../assets/images/icons/privilages/item_icon_01.svg" alt="Достоинства #01"></div>
+
+                    <p class="privilages__text">Благоприятный субтропический климат, уникальное территориальное расположение: Средиземное море с одной стороны и Торосские горы - с другой.</p>
+                </div>
+
+                <div class="privilages__item">
+                    <div class="privilages__icon"><img src="../assets/images/icons/privilages/item_icon_02.svg" alt="Достоинства #02"></div>
+
+                    <p class="privilages__text">Яркая культура, бесценное историческое наследие прошлого в сочетании с лучшими курортами европейского стандарта качества создают ощущение самобытности и исключительности жизни и отдыха.</p>
+                </div>
+
+                <div class="privilages__item">
+                    <div class="privilages__icon"><img src="../assets/images/icons/privilages/item_icon_03.svg" alt="Достоинства #03"></div>
+
+                    <p class="privilages__text">Большой экономический потенциал и активные темпы развития державы во всех сферах деятельности обеспечивают благоприятные перспективы уже сейчас для иностранных инвесторов. </p>
+                </div>
+
+                <div class="privilages__item">
+                    <div class="privilages__icon"><img src="../assets/images/icons/privilages/item_icon_04.svg" alt="Достоинства #04"></div>
+
+                    <p class="privilages__text">Легкое оформление вида на жительство и открытие банковского счета при покупке недвижимости от застройщика в Турции или вторичного жилья. </p>
+                </div>
+
+                <div class="privilages__item">
+                    <div class="privilages__icon"><img src="../assets/images/icons/privilages/item_icon_05.svg" alt="Достоинства #05"></div>
+
+                    <p class="privilages__text">Современная медицина, которая по праву считается одной из лучших в мире. Простое оформление страховки и условия пользования всем спектром предоставляемых услуг. </p>
+                </div>
+
+                <div class="privilages__item">
+                    <div class="privilages__icon"><img src="../assets/images/icons/privilages/item_icon_06.svg" alt="Достоинства #06"></div>
+
+                    <p class="privilages__text">Доступная стоимость проживания: платежи за коммунальные услуги, продукты, необходимые товары для жизни, медикаменты, сервисное обслуживание.</p>
+                </div>
+                
+            </div>
+
+        </div>
+    </section>
+
+    <!--== Benefit  -->
+    <section class="benefit-section benefit">
+        <div class="container">
+        
+        <div class="benefit__header">
+            <div class="benefit__header-image">
+                <h2 class="section-title benefit__title">Почему стоит приобрести недвижимость в Алании</h2>
+            </div>
+
+            <div class="benefit__header-text">
+                <h4 class="benefit__text-heading">Аланья – аутентичный курортный город на побережье Средиземного моря в Турции с невероятной атмосферой, в котором гармонично сочетаются восточный колорит и европейский уровень жизни.</h4>
+
+                <p class="benefit__text">Это место считается одним из самых перспективных в державе и сосредотачивает в себе все преимущества отдыха и проживания высокого уровня: уникальный климат - 300 солнечных дней в году, образцовый сервис, хорошо развитая инфраструктура, радушное местное население. </p>
+
+                <p class="benefit__text">К услугам отдыхающих и жителей города круглогодичный отдых: в теплый сезон – у моря, в зимнее время – на новом горнолыжном курорте в горах недалеко от города. А также увлекательный шопинг, большой выбор развлечений на любой вкус, престижные рестораны мировой и национальной кухни, уютные кафе, пляжи, обустроенные по самым высоким стандартам, спортивные комплексы, яркие ночные клубы, квалифицированные медицинские центры, а также разнообразие первоклассных учебных заведений (в том числе детских садов, школ и университетов с возможностью обучения на русском языке).</p>
+            </div>
+
+        </div>
+
+        <div class="benefit__content">
+        
+            <div class="benefit__item">
+                <div class="benefit__icon">
+                    <img src="../assets/images/icons/benefit/item_icon_01.svg" alt="Benefit Icon #01">
+                </div>
+                <p class="benefit__text"><strong>Удобное авиасообщение</strong> со многими городами стран СНГ, используя услуги двух расположенных рядом аэропортов Анталии и Аланьи. </p>
+
+                <p class="benefit__text">Продолжительность полета из России или Украины до Антальи или Алании в среднем занимает <strong>от 2 до 4 часов.</strong></p>
+
+                <p class="benefit__text">Курорт Алания предлагает <strong>европейский уровень комфорта</strong> и обслуживания, при этом стоимость проживания на <strong>60 % дешевле</strong>, нежели в Европе. </p>
+            </div>
+
+            <div class="benefit__item">
+                <div class="benefit__icon">
+                    <img src="../assets/images/icons/benefit/item_icon_02.svg" alt="Benefit Icon #02">
+                </div>
+                <p class="benefit__text"><strong>Низкие коммунальные тарифы и Айдат</strong>, включающий оплату за содержание жилого комплекса (от 10 € в зависимости от уровня комплекса).</p>
+
+                <p class="benefit__text">Показатели среднего роста цен на рынке недвижимости составляют <strong>до 7 % в год</strong>, в то время, как средний показатель по миру насчитывает <strong>2,7%.</strong></p>
+
+                <p class="benefit__text"><strong>Низкое налогообложение</strong> (годовой налог – 0,2% от кадастровой стоимости) и отсутствие бюрократических издержек. </p>
+            </div>
+
+            <div class="benefit__item">
+                <div class="benefit__icon">
+                    <img src="../assets/images/icons/benefit/item_icon_03.svg" alt="Benefit Icon #03">
+                </div>
+                <p class="benefit__text"><strong>Низкие процентные ставки по ипотечному кредиту в евро или долларах</strong>. Покупая квартиру в Аланье от застройщика можно оформить ипотеку в турецком банке на сумму до 50% от оценочной стоимости объекта под 11%-13% годовых на срок в среднем до 10 лет.</p>
+
+                <p class="benefit__text">Хорошая окупаемость вложений при сдаче в аренду апартаментов, получение стабильного пассивного дохода.</p>
+
+                <p class="benefit__text"><strong>Простая процедура оформления сделки</strong> и быстрое получение документов о праве собственности на недвижимость (в среднем занимает 3 дня).</p>
+            </div>
+        
+        </div>
+
+        
         </div>
     </section>
 
