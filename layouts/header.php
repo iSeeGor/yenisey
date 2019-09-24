f1185742d89ba44e25aa1e972ed730ec0ffac58f
iSeeGor <iseegor87@gmail.com>
2019-09-24 18:13:09 +0300

    Mobile Menu + Columns

--- layouts/header.php
+++ layouts/header.php
@@ -29,7 +29,77 @@
 <body class="body">
     <div class="page">
 
+        <div class="sidenav">
+            <ul class="mobile-menu">
+                <li class="mobile-menu__item"><a href="#" class="link mobile-menu__link" title="Главная">Главная</a></li>
+                <li class="mobile-menu__item mobile-menu__item_dropdown is-dropdown">
+                    <a href="#" class="link mobile-menu__link" title="Каталог недвижимости">Каталог недвижимости</a>
+
+                    <ul class="mobile-menu dropdown-menu">
+                        <li class="dropdown-menu__item"><a href="#" class="link dropdown-menu__link" title="Квартиры">Квартиры</a></li>
+                        <li class="dropdown-menu__item"><a href="#" class="link dropdown-menu__link" title="Виллы">Виллы</a></li>
+                        <li class="dropdown-menu__item"><a href="#" class="link dropdown-menu__link"
+                                title="Коммерческая недвижимость">Коммерческая недвижимость</a></li>
+                        <li class="dropdown-menu__item"><a href="#" class="link dropdown-menu__link" title="Аренда">Аренда</a></li>
+                    </ul>
+                </li>
+                <li class="mobile-menu__item mobile-menu__item_dropdown is-dropdown">
+                    <a href="#" class="link mobile-menu__link" title="Наши комплексы">Наши комплексы</a>
+
+                    <ul class="mobile-menu dropdown-menu">
+                        <li class="dropdown-menu__item"><a href="#" class="link dropdown-menu__link" title="Yenisey Residence">Yenisey Residence</a></li>
+                        <li class="dropdown-menu__item"><a href="#" class="link dropdown-menu__link" title="Yaparlı VI">Yaparlı VI</a></li>
+                        <li class="dropdown-menu__item"><a href="#" class="link dropdown-menu__link" title="Yenisey V">Yenisey V</a></li>
+                        <li class="dropdown-menu__item"><a href="#" class="link dropdown-menu__link" title="Yenisey IV A+B">Yenisey IV A+B</a></li>
+                        <li class="dropdown-menu__item"><a href="#" class="link dropdown-menu__link" title="Yenisey III">Yenisey III</a></li>
+                        <li class="dropdown-menu__item"><a href="#" class="link dropdown-menu__link" title="Yenisey II">Yenisey II</a></li>
+                        <li class="dropdown-menu__item"><a href="#" class="link dropdown-menu__link" title="Yenisey I">Yenisey I</a></li>
+                        <li class="dropdown-menu__item"><a href="#" class="link dropdown-menu__link" title="Yenisey Palace">Yenisey Palace</a></li>
+                        <li class="dropdown-menu__item"><a href="#" class="link dropdown-menu__link" title="Yenisey VII">Yenisey VII</a></li>
+                    </ul>
+                </li>
+                <li class="mobile-menu__item"><a href="#" class="link mobile-menu__link" title="О нас">О нас</a></li>
+                <li class="mobile-menu__item mobile-menu__item_dropdown is-dropdown">
+                    <a href="#" class="link mobile-menu__link" title="Для вас">Для вас</a>
+
+                    <ul class="mobile-menu dropdown-menu">
+                        <li class="dropdown-menu__item"><a href="#" class="link dropdown-menu__link" title="Услуги для клиентов">Услуги для
+                                клиентов</a></li>
+                        <li class="dropdown-menu__item"><a href="#" class="link dropdown-menu__link" title="Сотрудничество">Сотрудничество</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="mobile-menu__item"><a href="#" class="link mobile-menu__link">Полезные публикации</a></li>
+            </ul>
+
+            <ul class="phones__select-list_sidenav">
+                <li class="phones__item"><a href="tel:+902425283388" class="link phones__item-link phones__item-link_tur" title="+902425283388">+90 242 528 3388</a></li>
+                <li class="phones__item"><a href="tel:+902425283388" class="link phones__item-link phones__item-link_kaz" title="+902425283388">+90 242 528 3388</a></li>                                               
+                <li class="phones__item"><a href="tel:+902425283388" class="link phones__item-link phones__item-link_ukr" title="+902425283388">+38 098 600 7886</a></li>
+                <li class="phones__item"><a href="tel:+902425283388" class="link phones__item-link phones__item-link_rus" title="+902425283388">+90 242 528 3388</a></li>
+            </ul>
+
+            <button class="button button--blue button--small">Перезвоните мне</button>
+        </div>
+
         <header class="header">
+            <div class="mobile-header header__mobile-header">
+                <div class="logo mobile-logo">
+                    <a href="/" title="Yenisey Logo"><img src="../assets/images/yenisey_logo.png" alt="Yenisey Logo" width="117"></a>
+                </div>
+
+                <div class="hamberger">
+                    <svg class="hamberger__icon" width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M43.7057 39.7032H12.9457C11.6779 39.7032 10.6504 38.6757 10.6504 37.4079C10.6504 36.1402 11.6779 35.1126 12.9457 35.1126H43.7049C44.9727 35.1126 46.0002 36.1402 46.0002 37.4079C46.0002 38.6757 44.9735 39.7032 43.7057 39.7032Z" />
+                        <path d="M43.7057 25.2957H12.9457C11.6779 25.2957 10.6504 24.2682 10.6504 23.0004C10.6504 21.7326 11.6779 20.7051 12.9457 20.7051H43.7049C44.9727 20.7051 46.0002 21.7326 46.0002 23.0004C46.001 24.2682 44.9735 25.2957 43.7057 25.2957Z" />
+                        <path d="M43.7057 10.8881H12.9457C11.6779 10.8881 10.6504 9.86058 10.6504 8.59281C10.6504 7.32504 11.6779 6.29752 12.9457 6.29752H43.7049C44.9727 6.29752 46.0002 7.32504 46.0002 8.59281C46.0002 9.86058 44.9735 10.8881 43.7057 10.8881Z" />
+                        <path d="M3.08258 11.8529C4.78504 11.8529 6.16516 10.4728 6.16516 8.77033C6.16516 7.06786 4.78504 5.68774 3.08258 5.68774C1.38012 5.68774 0 7.06786 0 8.77033C0 10.4728 1.38012 11.8529 3.08258 11.8529Z" />
+                        <path d="M3.08258 26.083C4.78504 26.083 6.16516 24.7029 6.16516 23.0004C6.16516 21.2979 4.78504 19.9178 3.08258 19.9178C1.38012 19.9178 0 21.2979 0 23.0004C0 24.7029 1.38012 26.083 3.08258 26.083Z" />
+                        <path d="M3.08258 40.313C4.78504 40.313 6.16516 38.9329 6.16516 37.2304C6.16516 35.528 4.78504 34.1479 3.08258 34.1479C1.38012 34.1479 0 35.528 0 37.2304C0 38.9329 1.38012 40.313 3.08258 40.313Z" />
+                    </svg>
+                </div>
+            </div>
+
             <div class="topbar header__topbar">
                 <div class="container topbar__container">
                     <div class="row">
@@ -45,7 +115,7 @@
                                     </ul>
                                 </div>
                                 <div class="logo">
-                                    <a href="/" title="Yenisey Logo"><img src="../assets/images/yenisey_logo.png" alt="Yenisey Logo"></a>
+                                    <a href="/" title="Yenisey Logo"><img src="../assets/images/yenisey_logo.png" alt="Yenisey Logo" width="142"></a>
                                 </div>
                                 <button class="button button--blue button--small">Перезвоните мне</button>
                             </div>    
